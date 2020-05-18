dcf00aef152ca9adcd84a304c29a8789a7fe7b5e
Alena <af@Alenas-MacBook-Pro.local>
2020-05-18 20:41:45 +0100

    fixed images

--- scenes/StartScene.ts
+++ scenes/StartScene.ts
@@ -11,28 +11,28 @@ export class StartScene extends BasicScene {
     }
 
     preload() {
-        this.load.spritesheet('catrun', '../assets/run.png', {
+        this.load.spritesheet('catrun', 'assets/run.png', {
             frameWidth: 296,
             frameHeight: 430
         })
-        this.load.spritesheet('catidle', '../assets/idle.png', {
+        this.load.spritesheet('catidle', 'assets/idle.png', {
             frameWidth: 291,
             frameHeight: 434
         })
-        this.load.spritesheet('cat', '../assets/catjumping.png', {
+        this.load.spritesheet('cat', 'assets/catjumping.png', {
             frameWidth: 200,
             frameHeight: 175
         })
-        this.load.image('grass', '../assets/grass.png')
-        this.load.image('sky', '../assets/sky.png')
-        this.load.image('ground', '../assets/ground.png')
-        this.load.spritesheet('door', '../assets/door.png', {
+        this.load.image('grass', 'assets/grass.png')
+        this.load.image('sky', 'assets/sky.png')
+        this.load.image('ground', 'assets/ground.png')
+        this.load.spritesheet('door', 'assets/door.png', {
             frameWidth: 48,
             frameHeight: 48
         })
 
-        this.load.image('platform', '../assets/platform.png')
-        this.load.image('star', '../assets/star.png')
+        this.load.image('platform', 'assets/platform.png')
+        this.load.image('star', 'assets/star.png')
     }
 
     onCreate(): void {
